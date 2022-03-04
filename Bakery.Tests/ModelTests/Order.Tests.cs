f091bcfd9d7144abb8d3cc53e68863a197b7382b
Matt Eilar <meilar@gmail.com>
2022-03-04 08:38:03 -0800

    initialize project files and structure

--- Bakery.Tests/ModelTests/Order.Tests.cs
+++ Bakery.Tests/ModelTests/Order.Tests.cs
@@ -9,8 +9,7 @@ namespace ProjectName.Tests
     [TestMethod]
 public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
 {
-  // any necessary logic to prep for test; instantiating new classes, etc.
-  Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+
 }
   }
 }
