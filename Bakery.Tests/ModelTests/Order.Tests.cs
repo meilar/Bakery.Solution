5c9e897817b861c34bb5f4d5e705e6e5519a7034
Matt Eilar <meilar@gmail.com>
2022-03-04 08:57:32 -0800

    add order constructor and test

--- Bakery.Tests/ModelTests/Order.Tests.cs
+++ Bakery.Tests/ModelTests/Order.Tests.cs
@@ -1,15 +1,84 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProjectName;
+using System.Collections.Generic;
+using Bakery.Models;
+using System;
 
-namespace ProjectName.Tests
+namespace Bakery.Tests
 {
   [TestClass]
-  public class ClassNameTests
+  public class OrderTests : IDisposable
   {
-    [TestMethod]
-public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
-{
 
-}
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newItem = new Order(12, "two bread", "2022-06-20", "for customer's birthday");
+      Assert.AreEqual(typeof(Order), newItem.GetType());
+    }
+
+  //   [TestMethod]
+  //   public void GetDescription_ReturnsDescription_String()
+  //   {
+  //     //Arrange
+  //     string description = "Walk the dog.";
+
+  //     //Act
+  //     Order newItem = new Order(description);
+  //     string result = newItem.Description;
+
+  //     //Assert
+  //     Assert.AreEqual(description, result);
+  //   }
+
+  //   [TestMethod]
+  //   public void SetDescription_SetDescription_String()
+  //   {
+  //     //Arrange
+  //     string description = "Walk the dog.";
+  //     Order newItem = new Order(description);
+
+  //     //Act
+  //     string updatedDescription = "Do the dishes";
+  //     newItem.Description = updatedDescription;
+  //     string result = newItem.Description;
+
+  //     //Assert
+  //     Assert.AreEqual(updatedDescription, result);
+  //   }
+
+  //   [TestMethod]
+  //   public void GetAll_ReturnsEmptyList_ItemList()
+  //   {
+  //     // Arrange
+  //     List<Order> newList = new List<Order> { };
+
+  //     // Act
+  //     List<Order> result = Order.GetAll();
+
+  //     // Assert
+  //     CollectionAssert.AreEqual(newList, result);
+  //   }
+
+  //   [TestMethod]
+  //   public void GetAll_ReturnsItems_ItemList()
+  //   {
+  //     //Arrange
+  //     string description01 = "Walk the dog";
+  //     string description02 = "Wash the dishes";
+  //     Order newItem1 = new Order(description01);
+  //     Order newItem2 = new Order(description02);
+  //     List<Order> newList = new List<Order> { newItem1, newItem2 };
+
+  //     //Act
+  //     List<Order> result = Order.GetAll();
+
+  //     //Assert
+  //     CollectionAssert.AreEqual(newList, result);
+  //   }
   }
 }
---
+++ Bakery/Models/Order.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+
+namespace Bakery.Models
+{
+  public class Order
+  {
+    public int Amount { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+    public string Due { get; set; }
+    public string Notes { get; set; }
+    private static List<Order> _instances = new List<Order> { };
+
+    public Order(int amount, string description, string due, string notes)
+    {
+      Amount = amount;
+      Description = description;
+      Due = due;
+      Notes = notes;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    // public static List<Order> GetAll()
+    // {
+    //   return _instances;
+    // }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    // public static Order Find(int searchId)
+    // {
+    //   return _instances[searchId-1];
+    // }
+  }
+}
